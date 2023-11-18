debca35a82d08c1860321e99f121c5d976c34b1f
David Rosenbaum <DaveR1226@gmail.com>
2023-11-18 09:16:51 -0600

    Moved Spotify code into its own directory

--- src/spotify/authCodeWithPkce.ts
+++ src/spotify/authCodeWithPkce.ts
@@ -37,7 +37,7 @@ export async function getAccessToken(clientId: string, code: string) {
 
 function generateCodeVerifier(length: number) {
     let text = '';
-    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
     for (let i = 0; i < length; i++) {
         text += possible.charAt(Math.floor(Math.random() * possible.length));
