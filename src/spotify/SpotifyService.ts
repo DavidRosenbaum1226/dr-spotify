8ff6938e4509b0390e01818c485eafd45e243651
David Rosenbaum <DaveR1226@gmail.com>
2023-11-18 10:47:52 -0600

    Moved login.ts into SpotifyService.ts

--- src/spotify/SpotifyService.ts
+++ src/spotify/SpotifyService.ts
@@ -1,3 +1,4 @@
+const CLIENT_ID = "6d20d69320624236b7bf5ab52a530966";
 const REDIRECT_URL = "http://localhost:5173/callback";
 
 export async function redirectToAuthCodeFlow(clientId: string) {
@@ -55,3 +56,36 @@ async function generateCodeChallenge(codeVerifier: string) {
         .replace(/\//g, '_')
         .replace(/=+$/, '');
 }
+
+// async function doLogin() {
+//     const params = new URLSearchParams(window.location.search);
+//     const code = params.get("code");
+
+//     if (!code) {
+//         redirectToAuthCodeFlow(CLIENT_ID);
+//     } else {
+//         const accessToken = await getAccessToken(CLIENT_ID, code);
+//         const profile = await fetchProfile(accessToken);
+//         populateUI(profile);
+//     }
+// }
+
+// async function fetchProfile(code: string): Promise<UserProfile> {
+//     const result = await fetch("https://api.spotify.com/v1/me", {
+//         method: "GET", headers: { Authorization: `Bearer ${code}` }
+//     });
+
+//     return await result.json();
+// }
+
+// function populateUI(profile: UserProfile) {
+//     document.getElementById("displayName")!.innerText = profile.display_name;
+//     document.getElementById("avatar")!.setAttribute("src", profile.images[0].url)
+//     document.getElementById("id")!.innerText = profile.id;
+//     document.getElementById("email")!.innerText = profile.email;
+//     document.getElementById("uri")!.innerText = profile.uri;
+//     document.getElementById("uri")!.setAttribute("href", profile.external_urls.spotify);
+//     document.getElementById("url")!.innerText = profile.href;
+//     document.getElementById("url")!.setAttribute("href", profile.href);
+//     document.getElementById("imgUrl")!.innerText = profile.images[0].url;
+// }
