88800b31656d7939ba22b572c5ddf96690cf843e
David Rosenbaum <DaveR1226@gmail.com>
2023-12-19 11:04:20 -0600

    Tweaked AppRouter

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,24 +1,23 @@
-// AppRouter.tsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
-import LoginCallback from './LoginCallback';
+import LoginCallback from './spotify/LoginCallback';
 import ErrorPage from './ErrorPage';
 
 const AppRouter: React.FC = () => {
   return (
     <Router>
-      <Switch>
+      <Routes>
+        <Route path="/">
+          <App />
+        </Route>
         <Route path="/login-callback">
           <LoginCallback />
         </Route>
         <Route path="/error">
           <ErrorPage />
         </Route>
-        <Route path="/">
-          <App />
-        </Route>
-      </Switch>
+      </Routes>
     </Router>
   );
 };
