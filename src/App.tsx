75a60c57cd55878a35f76cf4136f08711234a3ea
David Rosenbaum <DaveR1226@gmail.com>
2023-12-28 09:13:41 -0600

    Added useEffect hook for App page load

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ACCESS_TOKEN_KEY, redirectToAuthorizeUrl, fetchAccessToken } from './spotify/SpotifyService';
 
 const App: React.FC = () => {
   console.log('App render');
 
+  const [accessToken, setAccessToken] = useState<string | null>(null);
   // const [userProfile, setUserProfile] = useState<UserProfile | null>(null);
 
-  // If being redirected back from Spotify with a code, fetch the access token
-  const params = new URLSearchParams(window.location.search);
-  const codeParam = params.get("code");
-  if (codeParam) {
-    fetchAccessToken(codeParam)
-      .then((token) => { if (token) { localStorage.setItem(ACCESS_TOKEN_KEY, token) } })
-      .catch((error) => console.error('Error fetching access token: ', error));
-  }
+  useEffect(() => {
+    // If being redirected back from Spotify with a code, fetch the access token
+    const params = new URLSearchParams(window.location.search);
+    const codeParam = params.get("code");
+    if (codeParam) {
+      fetchAccessToken(codeParam)
+        .then((token) => { if (token) { localStorage.setItem(ACCESS_TOKEN_KEY, token) } })
+        .catch((error) => console.error('Error fetching access token: ', error));
+    }
 
-  const accessToken = localStorage.getItem(ACCESS_TOKEN_KEY);
-  if (!accessToken) {
-    redirectToAuthorizeUrl();
-  }
+    const token = localStorage.getItem(ACCESS_TOKEN_KEY);
+    if (!token) {
+      redirectToAuthorizeUrl();
+    } else {
+      setAccessToken(token);
+    }
 
-  // if (accessToken && !userProfile) {
-  //   getUserProfile(accessToken)
-  //     .then((data) => setUserProfile(data))
-  //     .catch((error) => {
-  //       console.error('Error fetching profile: ', error);
-  //       // redirectToAuthorizeUrl()
-  //     });
-  // }
+    // if (accessToken && !userProfile) {
+    //   getUserProfile(accessToken)
+    //     .then((data) => setUserProfile(data))
+    //     .catch((error) => {
+    //       console.error('Error fetching profile: ', error);
+    //       // redirectToAuthorizeUrl()
+    //     });
+    // }
+  }, []);
 
   return (
     <div>
-      <h2>Code: {codeParam}, Access token: {accessToken}</h2>
+      <h2>Access token: {accessToken}</h2>
     </div>
   );
-};
+}
 
 export default App;
