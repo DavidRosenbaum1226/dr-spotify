e92df9d34491d67685eced34600bb3e066faae62
David Rosenbaum <DaveR1226@gmail.com>
2024-01-12 12:28:18 -0600

    Improved example data in Playlists and Playlist

--- src/components/Playlist.tsx
+++ src/components/Playlist.tsx
@@ -1,5 +1,21 @@
 import { SpotifyApi } from "@spotify/web-api-ts-sdk";
+import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid';
+
+const rows: GridRowsProp = [
+    { id: 1, song: 'Blinding Lights', artist: 'The Weeknd' },
+    { id: 2, song: 'Dance Monkey', artist: 'Tones and I' },
+    { id: 3, song: 'Watermelon Sugar', artist: 'Harry Styles' },
+];
+
+const columns: GridColDef[] = [
+    { field: 'song', headerName: 'Song', width: 150 },
+    { field: 'artist', headerName: 'Artist', width: 150 },
+];
 
 export const Playlist = ({ sdk }: { sdk: SpotifyApi }) => {
-    return <></>
+    return (
+        <div style={{ height: '80vh', width: '40%', maxWidth: '100%' }}>
+            <DataGrid rows={rows} columns={columns} />
+        </div>
+    )
 }
--- src/components/Playlists.tsx
+++ src/components/Playlists.tsx
@@ -2,20 +2,18 @@ import { SpotifyApi } from "@spotify/web-api-ts-sdk";
 import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid';
 
 const rows: GridRowsProp = [
-    { id: 1, col1: 'Hello', col2: 'World' },
-    { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
-    { id: 3, col1: 'MUI', col2: 'is Amazing' },
+    { id: 1, playlist: 'Chill Vibes' },
+    { id: 2, playlist: 'Party Mix' },
+    { id: 3, playlist: 'Workout Jams' },
 ];
 
 const columns: GridColDef[] = [
-    { field: 'col1', headerName: 'Column 1', width: 150 },
-    { field: 'col2', headerName: 'Column 2', width: 150 },
+    { field: 'playlist', headerName: 'Playlist', width: 150 },
 ];
-
 export const Playlists = ({ sdk }: { sdk: SpotifyApi }) => {
     return (
-        <>
+        <div style={{ height: '80vh', width: '40%', maxWidth: '100%' }}>
             <DataGrid rows={rows} columns={columns} />
-        </>
+        </div>
     )
 }
