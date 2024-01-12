3f1e08eabf1fd313e8e2badec8feb44c55f82145
David Rosenbaum <DaveR1226@gmail.com>
2024-01-12 12:03:56 -0600

    Simplified Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { SpotifyApi, UserProfile } from "@spotify/web-api-ts-sdk";
-import { AppBar, Avatar, Box, Stack } from "@mui/material";
+import { Avatar, Box, Stack } from "@mui/material";
 
 export const Header = ({ sdk }: { sdk: SpotifyApi }) => {
     const [userProfile, setUserProfile] = useState<UserProfile | null>(null);
@@ -13,11 +13,9 @@ export const Header = ({ sdk }: { sdk: SpotifyApi }) => {
     }, [sdk]);
 
     return (
-        <AppBar>
-            <Stack direction="row">
-                <Avatar sx={{ m: 0.5 }} alt={userProfile?.display_name} src={userProfile?.images[0]?.url} />
-                <Box sx={{ m: 0.5 }}>{userProfile?.display_name}</Box>
-            </Stack>
-        </AppBar>
+        <Stack direction="row">
+            <Avatar sx={{ m: 0.5 }} alt={userProfile?.display_name} src={userProfile?.images[0]?.url} />
+            <Box sx={{ m: 0.5 }}>{userProfile?.display_name}</Box>
+        </Stack>
     );
 }
