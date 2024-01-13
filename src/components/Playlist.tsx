7c5154e37065f7226ecbf59d9e4970f913ccf7d8
David Rosenbaum <DaveR1226@gmail.com>
2024-01-12 19:20:17 -0600

    Added example rows

--- src/components/Playlist.tsx
+++ src/components/Playlist.tsx
@@ -5,6 +5,23 @@ const rows: GridRowsProp = [
     { id: 1, song: 'Blinding Lights', artist: 'The Weeknd' },
     { id: 2, song: 'Dance Monkey', artist: 'Tones and I' },
     { id: 3, song: 'Watermelon Sugar', artist: 'Harry Styles' },
+    { id: 4, song: 'Shape of You', artist: 'Ed Sheeran' },
+    { id: 5, song: 'Uptown Funk', artist: 'Mark Ronson ft. Bruno Mars' },
+    { id: 6, song: 'Closer', artist: 'The Chainsmokers ft. Halsey' },
+    { id: 7, song: 'Thinking Out Loud', artist: 'Ed Sheeran' },
+    { id: 8, song: 'Despacito', artist: 'Luis Fonsi ft. Daddy Yankee' },
+    { id: 9, song: 'Somebody That I Used to Know', artist: 'Gotye ft. Kimbra' },
+    { id: 10, song: 'Happy', artist: 'Pharrell Williams' },
+    { id: 11, song: 'Bad Guy', artist: 'Billie Eilish' },
+    { id: 12, song: 'Old Town Road', artist: 'Lil Nas X ft. Billy Ray Cyrus' },
+    { id: 13, song: 'Radioactive', artist: 'Imagine Dragons' },
+    { id: 14, song: 'Counting Stars', artist: 'OneRepublic' },
+    { id: 15, song: 'Rolling in the Deep', artist: 'Adele' },
+    { id: 16, song: 'Shallow', artist: 'Lady Gaga ft. Bradley Cooper' },
+    { id: 17, song: 'All of Me', artist: 'John Legend' },
+    { id: 18, song: 'Can\'t Stop the Feeling!', artist: 'Justin Timberlake' },
+    { id: 19, song: 'Wrecking Ball', artist: 'Miley Cyrus' },
+    { id: 20, song: 'Royals', artist: 'Lorde' },
 ];
 
 const columns: GridColDef[] = [
@@ -14,7 +31,7 @@ const columns: GridColDef[] = [
 
 export const Playlist = ({ sdk }: { sdk: SpotifyApi }) => {
     return (
-        <div style={{ height: '100vh' }}>
+        <div>
             <DataGrid rows={rows} columns={columns} />
         </div>
     )
--- src/components/Playlists.tsx
+++ src/components/Playlists.tsx
@@ -5,6 +5,18 @@ const rows: GridRowsProp = [
     { id: 1, playlist: 'Chill Vibes' },
     { id: 2, playlist: 'Party Mix' },
     { id: 3, playlist: 'Workout Jams' },
+    { id: 4, playlist: 'Study Beats' },
+    { id: 5, playlist: 'Road Trip Tunes' },
+    { id: 6, playlist: 'Relaxing Piano' },
+    { id: 7, playlist: 'Rock Classics' },
+    { id: 8, playlist: 'Hip Hop Hits' },
+    { id: 9, playlist: 'Indie Favorites' },
+    { id: 10, playlist: 'Throwback Anthems' },
+    { id: 11, playlist: 'EDM Bangers' },
+    { id: 12, playlist: 'Country Hits' },
+    { id: 13, playlist: 'R&B Grooves' },
+    { id: 14, playlist: 'Pop Hits' },
+    { id: 15, playlist: 'Jazz Essentials' },
 ];
 
 const columns: GridColDef[] = [
@@ -13,7 +25,7 @@ const columns: GridColDef[] = [
 
 export const Playlists = ({ sdk }: { sdk: SpotifyApi }) => {
     return (
-        <div style={{ height: '100vh' }}>
+        <div>
             <DataGrid rows={rows} columns={columns} />
         </div>
     )
