ba1f5c68001e1297ad681693a8c914707d8f8c4b
David Rosenbaum <DaveR1226@gmail.com>
2024-01-15 13:30:07 -0600

    Implementing UserPlaylists

--- src/components/UserPlaylists.tsx
+++ src/components/UserPlaylists.tsx
@@ -1,42 +1,33 @@
 import { useEffect, useState } from "react";
-import { Page, SimplifiedPlaylist, SpotifyApi } from "@spotify/web-api-ts-sdk";
-import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid';
-
-const rows: GridRowsProp = [
-    { id: 1, playlist: 'Chill Vibes' },
-    { id: 2, playlist: 'Party Mix' },
-    { id: 3, playlist: 'Workout Jams' },
-    { id: 4, playlist: 'Study Beats' },
-    { id: 5, playlist: 'Road Trip Tunes' },
-    { id: 6, playlist: 'Relaxing Piano' },
-    { id: 7, playlist: 'Rock Classics' },
-    { id: 8, playlist: 'Hip Hop Hits' },
-    { id: 9, playlist: 'Indie Favorites' },
-    { id: 10, playlist: 'Throwback Anthems' },
-    { id: 11, playlist: 'EDM Bangers' },
-    { id: 12, playlist: 'Country Hits' },
-    { id: 13, playlist: 'R&B Grooves' },
-    { id: 14, playlist: 'Pop Hits' },
-    { id: 15, playlist: 'Jazz Essentials' },
-];
+import { SpotifyApi } from "@spotify/web-api-ts-sdk";
+import { DataGrid, GridColDef, GridValidRowModel } from "@mui/x-data-grid";
 
 const columns: GridColDef[] = [
-    { field: 'playlist', headerName: 'Playlist', width: 150 },
+    { field: "playlist", headerName: "Playlist", width: 150 },
 ];
 
 export const UserPlaylists = ({ sdk }: { sdk: SpotifyApi }) => {
-    const [userPlaylistPage, setUserPlaylistPage] = useState<Page<SimplifiedPlaylist> | null>(null);
+    const [userPlaylistCount, setUserPlaylistCount] = useState<number>(0);
+    const [userPlaylistPageRows, setUserPlaylistPageRows] = useState<GridValidRowModel[]>([]);
 
     useEffect(() => {
         (async () => {
             const userPlaylists = await sdk.currentUser.playlists.playlists(0);
-            setUserPlaylistPage(() => userPlaylists);
+            setUserPlaylistCount(userPlaylists.total);
+            setUserPlaylistPageRows(userPlaylists.items.map(item => ({
+                id: item.id,
+                playlist: item.name,
+            })));
         })();
     }, [sdk]);
 
     return (
         <>
-            <DataGrid paginationMode='server' rows={rows} columns={columns} />
+            <DataGrid
+                paginationMode="server"
+                rowCount={userPlaylistCount}
+                rows={userPlaylistPageRows}
+                columns={columns} />
         </>
     )
 }
