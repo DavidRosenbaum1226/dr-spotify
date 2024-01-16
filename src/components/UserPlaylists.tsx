cd5dba2b4db6072687acf132f57a774322417e10
David Rosenbaum <DaveR1226@gmail.com>
2024-01-16 10:37:58 -0600

    Implementing playlist fetching

--- src/components/UserPlaylists.tsx
+++ src/components/UserPlaylists.tsx
@@ -1,32 +1,42 @@
-import { useEffect, useState } from "react";
-import { SpotifyApi } from "@spotify/web-api-ts-sdk";
-import { DataGrid, GridColDef, GridValidRowModel } from "@mui/x-data-grid";
+import { useCallback, useEffect, useState } from "react";
+import { MaxInt, SpotifyApi } from "@spotify/web-api-ts-sdk";
+import { DataGrid, GridColDef, GridPaginationModel, GridValidRowModel } from "@mui/x-data-grid";
 
 const columns: GridColDef[] = [
     { field: "playlist", headerName: "Playlist", width: 150 },
 ];
 
 export const UserPlaylists = ({ sdk }: { sdk: SpotifyApi }) => {
+    const [paginationModel, setPaginationModel] = useState<GridPaginationModel>({ page: 0, pageSize: 5 })
     const [userPlaylistCount, setUserPlaylistCount] = useState<number>(0);
     const [userPlaylistPageRows, setUserPlaylistPageRows] = useState<GridValidRowModel[]>([]);
 
-    useEffect(() => {
-        (async () => {
-            const userPlaylists = await sdk.currentUser.playlists.playlists(0);
-            setUserPlaylistCount(userPlaylists.total);
-            setUserPlaylistPageRows(userPlaylists.items.map(item => ({
-                id: item.id,
-                playlist: item.name,
-            })));
-        })();
+    const fetchPage = useCallback(async (model: GridPaginationModel) => {
+        const userPlaylists = await sdk.currentUser.playlists.playlists(model.page as MaxInt<50>, model.pageSize);
+        setUserPlaylistCount(userPlaylists.total);
+        setUserPlaylistPageRows(userPlaylists.items.map(item => ({
+            id: item.id,
+            playlist: item.name,
+        })));
     }, [sdk]);
+
+
+    const handlePaginationModelChange = (model: GridPaginationModel) => {
+        setPaginationModel(model);
+        fetchPage(model);
+    };
+
+    useEffect(() => {
+        (async () => await fetchPage(paginationModel))();
+    }, [fetchPage, paginationModel, sdk]);
 
     return (
         <>
             <DataGrid
                 paginationMode="server"
+                paginationModel={paginationModel}
+                onPaginationModelChange={handlePaginationModelChange}
                 pageSizeOptions={[5, 10, 25]}
-                autoPageSize
                 rowCount={userPlaylistCount}
                 rows={userPlaylistPageRows}
                 columns={columns} />
